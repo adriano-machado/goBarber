97e99ab675a53403810ab8bd0e933f55da1206f8
Adriano Machado <adriano.machado@somagrupo.com.br>
2019-07-07 18:38:35 -0300

    jwt working

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,32 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const authConfig = require('../../config/auth');
 
 class SessionController {
-    async store(req, res) {}
+    async store(req, res) {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Usuário não cadastrado' });
+        }
+
+        if (!(await user.checkPassword(password))) {
+            return res.status(401).json({ error: 'Senha incorreta' });
+        }
+
+        const { id, name } = user;
+        return res.status(200).json({
+            user: {
+                id,
+                name,
+                email,
+            },
+            token: jwt.sign({ id }, authConfig.secret, {
+                expiresIn: authConfig.expiresIn,
+            }),
+        });
+    }
 }
 module.exports = new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const UserController = require('./app/controllers/UserController');
+const SessionController = require('./app/controllers/SessionController');
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
 
 module.exports = routes;
