63daf1e738c053fd9289acaaa350fdf474c0ed09
Adriano Machado <adriano.machado@somagrupo.com.br>
2019-07-11 16:11:23 -0300

    validation with Yup

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,17 @@
 const jwt = require('jsonwebtoken');
+const Yup = require('yup');
 const User = require('../models/User');
 const authConfig = require('../../config/auth');
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().required(),
+            password: Yup.string().required(),
+        });
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation error' });
+        }
         const { email, password } = req.body;
 
         const user = await User.findOne({ where: { email } });
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,20 @@
+const Yup = require('yup');
 const User = require('../models/User');
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().required(),
+            password: Yup.string()
+                .required()
+                .min(6),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation error' });
+        }
+
         const userExists = await User.findOne({
             where: { email: req.body.email },
         });
@@ -11,15 +24,37 @@ class UserController {
         const user = await User.create(req.body);
         const { id, name, provider, email } = user;
 
-        return res.json({ id, name, provider, email });
+        return res.status(200).json({ id, name, provider, email });
     }
 
     async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string()
+                .min(6)
+                .when('oldPassword', (oldPassword, field) => {
+                    return oldPassword ? field.required() : field;
+                }),
+            confirm_password: Yup.string().when(
+                'password',
+                (password, field) => {
+                    return password
+                        ? field.required().oneOf([Yup.ref('password')])
+                        : field;
+                }
+            ),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation error' });
+        }
         const { email, oldPassword } = req.body;
 
         const user = await User.findByPk(req.userId);
 
-        if (email !== user.email) {
+        if (email && email !== user.email) {
             const userExists = await User.findOne({
                 where: { email },
             });
