48b98d7eae5efe32e3ca95b318a489f5978f4b5e
Adriano Machado <adriano.machado@somagrupo.com.br>
2019-07-14 20:57:10 -0300

    validações de data

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 const Yup = require('yup');
+const { startOfHour, parseISO, isBefore } = require('date-fns');
 const Appointment = require('../models/Appointment');
 const User = require('../models/User');
 
@@ -27,6 +28,32 @@ class AppointmentController {
             return res.status(401).json({
                 error: 'You can only creat appointments with providers',
             });
+        }
+        /**
+         * check for Past Dates
+         */
+        const hourStart = startOfHour(parseISO(date));
+        if (isBefore(hourStart, new Date())) {
+            return res
+                .status(400)
+                .json({ error: 'Past date are not permited' });
+        }
+
+        /**
+         * Check date availability
+         */
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkAvailability) {
+            return res
+                .status(400)
+                .json({ error: 'Appointment date is not available' });
         }
 
         const appointment = await Appointment.create({
