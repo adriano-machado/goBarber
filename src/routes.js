9c2e165b0833959eb0c494b16034b96d6c281419
Adriano Machado <adriano.machado@somagrupo.com.br>
2019-07-14 17:32:29 -0300

    listando prestadores de serviço e criando rota para acessarmos imagens

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const UserController = require('./app/controllers/UserController');
 const FileController = require('./app/controllers/FileController');
 
 const SessionController = require('./app/controllers/SessionController');
+const ProviderController = require('./app/controllers/ProviderController');
+
 const authMiddleware = require('./app/middlewares/auth');
 const multerConfig = require('./config/multer');
 
@@ -14,9 +16,10 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
-routes.use(authMiddleware);
 routes.put('/users', UserController.update);
-
+routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
+
+// routes.use(authMiddleware);
 
 module.exports = routes;
